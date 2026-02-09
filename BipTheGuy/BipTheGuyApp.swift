224d915ab2248f95f1d4b34cc90df191e3611d14
Hyunjin Kim <hyun.jin.kim.1112@gmail.com>
2026-02-09 15:11:14 -0500

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by Hyunjin Kim on 9/2/26.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

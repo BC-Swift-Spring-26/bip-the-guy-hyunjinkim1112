060422abd880e83c87a3b9fd81341c9ba2a76aea
Hyunjin Kim <hyun.jin.kim.1112@gmail.com>
2026-02-09 09:48:26 -0500

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Hyunjin Kim on 9/2/26.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
